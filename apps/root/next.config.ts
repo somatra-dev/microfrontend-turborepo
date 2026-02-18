98e104c0e70c0e9912a26a5df4c3f136fa92ddaf
somatra-dev <somatrachey9527@gmail.com>
2026-02-18 16:08:42 +0700

    remove route from route(3000)

--- apps/root/next.config.ts
+++ apps/root/next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const iphoneDomain = process.env.IPHONE_DOMAIN ?? "http://localhost:3001";
-const ipadDomain = process.env.IPAD_DOMAIN ?? "http://localhost:3002";
-const macbookDomain = process.env.MACBOOK_DOMAIN ?? "http://localhost:3003";
 
 const nextConfig: NextConfig = {
   images: {
@@ -12,53 +9,7 @@ const nextConfig: NextConfig = {
         hostname: "i.pinimg.com",
       },
     ],
-  },
-  async rewrites() {
-    return [
-
-      // for profile routing
-      {
-        source: "/iphone/profile",
-        destination: "/profile",
-      },
-      {
-        source: "/iphone/profile/:path+",
-        destination: "/profile/:path+",
-      },
-      {
-        source: "/ipad/profile",
-        destination: "/profile",
-      },
-      {
-        source: "/ipad/profile/:path+",
-        destination: "/profile/:path+",
-      },
-      {
-        source: "/macbook/profile",
-        destination: "/profile",
-      },
-      {
-        source: "/macbook/profile/:path+",
-        destination: "/profile/:path+",
-      },
-
-      //------------------------------
-      {
-        source: "/iphone-static/:path+",
-        destination: `${iphoneDomain}/iphone-static/:path+`,
-      },
-
-      {
-        source: "/ipad-static/:path+",
-        destination: `${ipadDomain}/ipad-static/:path+`,
-      },
-
-      {
-        source: "/macbook-static/:path+",
-        destination: `${macbookDomain}/macbook-static/:path+`,
-      },
-    ];
-  },
+  }
 };
 
 export default nextConfig;
